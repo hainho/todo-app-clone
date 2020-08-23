c0cb66667e972ad51a976a58362a0623626e550e
Ha In Ho <hainho9704@gmail.com>
2020-08-23 22:36:12 +0900

    just clone

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import Header from './Header'
+import Body from './Body'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    todos: []
+  }
+
+ // 할일 추가 함수
+ addTodo = (todo) => {
+     
+  // 새로운 할일(todo) 객체 생성
+  const newTodo = {
+      id: Date.now(), // 등록시간
+      text: todo,      // 할일 내용
+      completed: false, // 완료 여부
+  }   
+
+  // state 업데이트
+  this.setState(prevState => ({
+      todos: [
+          newTodo,       // 새로 추가된 할일(todo)
+          ...prevState.todos // 기존의 할일 목록
+      ]
+  }));
+ 
+  // 콘솔창에 출력해보자~
+  console.log(this.state.todos);
+}
+
+checkTodo = (id) => {
+  this.setState(prevState => {
+    const [ todo ] = prevState.todos.filter(e => e.id === id);
+    todo.completed = !todo.completed;
+    return ({
+      todos: [
+          ...prevState.todos
+      ]
+    })
+  });
+}
+
+removeTodo = (id) => {
+  this.setState(prevState => {
+    const index = prevState.todos.findIndex(e => e.id === id);
+    prevState.todos.splice(index, 1);
+    return ({
+      todos: [
+          ...prevState.todos
+      ]
+    })
+  });
+}
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Todo App</Text>
+        <Header addTodo={this.addTodo}/>
+        <Body 
+          todos={this.state.todos} 
+          checkTodo={this.checkTodo} 
+          removeTodo={this.removeTodo}  />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    paddingTop: 50,
+    backgroundColor: "#EEE",
   },
+  title: {
+    fontWeight: "800",
+    fontSize: 30, 
+    marginLeft: 20,
+    marginBottom: 20,
+  }
 });
